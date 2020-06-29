2a164f5aba7bfd8a349b2175a1ebf546c98a33e8
lawa0921 <bag571ivy3470@gmail>
2020-06-29 14:59:46 +0800

    post CRUD 邏輯粗製

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,47 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
 
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @post.destroy
+      redirect_to posts_path
+    else
+    end
+  end
+
+  def show
+  end
 
   private
+  def set_post
+    Post.find(params[:id])
+  end
+
   def post_params
     params.require(:post).permit(:content)
   end
