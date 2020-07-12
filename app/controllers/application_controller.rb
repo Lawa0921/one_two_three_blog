f3be62d061bd487610514dde8e00c706bb2c8ec5
lawa0921 <bag571ivy3470@gmail>
2020-07-12 21:49:52 +0800

    可以上傳頭貼

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :type, :nickname])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :type, :nickname])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :type, :nickname, :avatar])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :type, :nickname, :avatar])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :recoverable, :rememberable, :registerable
   has_many :site_users
   has_many :sites, through: :site_users
+  has_one_attached :avatar
 end
