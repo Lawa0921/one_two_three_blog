13d2a0908ef34dfe10cff4e65e4153867c1fa626
lawa0921 <bag571ivy3470@gmail>
2020-07-12 11:21:43 +0800

    site new page

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,4 +2,11 @@ class SitesController < ApplicationController
   def index
     @sites = Site.all.order("RANDOM()").limit(9)
   end
+
+  def create
+  end
+  
+  def new
+    @site = Site.new
+  end
 end
