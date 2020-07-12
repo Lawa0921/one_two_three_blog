0aff42ac444c2c0b98cae48b8b27e844b457f2f7
lawa0921 <bag571ivy3470@gmail>
2020-07-12 11:58:49 +0800

    site create

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,9 +4,20 @@ class SitesController < ApplicationController
   end
 
   def create
+    @site = current_user.sites.new(site_params)
+    if @site.save
+      redirect_to sites_path, notice: t("site.create")
+    else
+      render :new
+    end
   end
   
   def new
     @site = Site.new
+  end
+
+  private
+  def site_params
+    params.require(:site).permit(:title, :description)
   end
 end
